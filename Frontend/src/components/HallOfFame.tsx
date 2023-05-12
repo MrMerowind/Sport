637a7be02ea79c229b4e8fb1c27b81972ce61354
MrMerowind <mrmerowind@gmail.com>
2023-05-12 14:58:26 +0200

    Add sorting to score list.

--- Frontend/src/components/HallOfFame.tsx
+++ Frontend/src/components/HallOfFame.tsx
@@ -19,7 +19,7 @@ export default function HallOfFame() {
   return (
     <div className='window-small'>
         <div><span>Użytkownik:</span><span><span>Punkty</span></span></div>
-        {usersData.length > 0 ? (usersData.map(user => {
+        {usersData.length > 0 ? (usersData.sort((a,b) => {return (b.pushups * 10 + b.pullups * 25 + b.squats * 3 + b.situps * 3 + b.run * 20) - (a.pushups * 10 + a.pullups * 25 + a.squats * 3 + a.situps * 3 + a.run * 20)}).map(user => {
         
         return <div><span>{user.name}:</span><span><span>{user.pushups * 10 + user.pullups * 25 + user.squats * 3 + user.situps * 3 + user.run * 20}</span></span></div>
 
